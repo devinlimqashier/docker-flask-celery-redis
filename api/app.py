8bcb40eb96583628da1b0d1a8a3967add80c0560
Devin <devin.lim@qashier.com>
2022-11-21 11:33:51 +0800

    WIP redis

--- api/app.py
+++ api/app.py
@@ -1,15 +1,17 @@
 import celery.states as states
 from flask import Flask, Response
-from flask import url_for, jsonify
+from flask import url_for, jsonify, request
 from worker import celery
+import redis
 
 dev_mode = True
 app = Flask(__name__)
+r = redis.Redis(host="redis")
 
 
 @app.route('/add/<int:param1>/<int:param2>')
 def add(param1: int, param2: int) -> str:
-    task = celery.send_task('tasks.add', args=[param1, param2], kwargs={})
+    task = celery.send_task('tasks.add', args=[param1, param2], kwargs={}, queue='')
     response = f"<a href='{url_for('check_task', task_id=task.id, external=True)}'>check status of {task.id} </a>"
     return response
 
@@ -27,6 +29,24 @@ def check_task(task_id: str) -> str:
 def health_check() -> Response:
     return jsonify("OK")
 
+@app.route('/print', methods=['POST'])
+def printer():
+    body = request.json
+    ip = body.get('ip')
+    message = body.get('message')
+    task = celery.send_task(f'tasks.printer', args=[ip, message], kwargs={}, queue=getQueue(ip))
+    response = f"<a href='{url_for('check_task', task_id=task.id, external=True)}'>check status of {task.id} </a>"
+    return response
+
+def getQueue(ip: str):
+    print(r)
+    if not r.get("queue_pool"): r.json().set("queue_pool", [f"queue_{x}" for x in range(20)])
+    print(r.get("queue_pool"))
+    queue_pool = r.get("queue_pool")
+    queue = queue_pool.pop()
+    r.set("queue_pool", queue_pool)
+    r.set(ip, queue)
+    return queue
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
